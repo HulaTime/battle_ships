fadf62220584c283a083f1311772abd52dd1ee52
MaxProvin <maxprovin01@gmail.com>
2016-10-24 22:40:17 +0100

    player can place pieces vertically

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,17 @@
 class Game
 
-	attr_reader :player_1, :player_2, :p1_defense, :p2_board
+	attr_reader :player_1, :player_2, :p1_defense, :p2_defense
 
 	def initialize(player_1, player_2 = nil)
 		@player_1 = player_1
 		@player_2 if !player_2
 		@p1_defense = nil
-		@p2_board = nil
+		@p2_defense = nil
 	end
 
 	def start(new_board)
 		@p1_defense = new_board.p1
-		@p2_board = new_board.p2
+		@p2_defense = new_board.p2
 		print "Welcome #{@player_1}, you move first..."
 	end
 
@@ -35,6 +35,11 @@ class Game
 			for n in x[-1]..y[-1]
 				@p1_defense[(x[0] + n.to_s).to_sym] = type
 				@p1_defense[(x[0] + n.to_s).to_sym] = type
+			end
+		elsif x[-1] == y[-1]
+			for l in x[0]..y[0]
+				@p1_defense[(l + x[-1]).to_sym] = type
+				@p1_defense[(l + y[-1]).to_sym] = type
 			end
 		end
 	end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,19 @@ require 'game'
 describe Game do
 
 	subject(:game) { described_class.new(player_1, player_2) }
-	let(:game_board) { double :game_board, p1: {a1:nil,a2:nil,a3:nil,a4:nil},
-																				 p2: {a1:nil,a2:nil,a3:nil,a4:nil} }
+	
+	let(:game_board_h) { 
+		double :game_board_h,
+		  p1: {a1:nil,a2:nil,a3:nil,a4:nil},
+			p2: {a1:nil,a2:nil,a3:nil,a4:nil}
+		}
+
+	let(:game_board_v) { 
+		double :game_board_v,
+		  p1: {a1:nil,b1:nil,c1:nil,d1:nil},
+			p2: {a1:nil,a2:nil,a3:nil,a4:nil}
+		}
+
 	let(:player_1) { 'Max' }
 	let(:player_2) { double :player_2, name: '' }
 
@@ -22,21 +33,22 @@ describe Game do
 	end 
 
 	it 'An initialized game generates a message once started' do
-		expect { game.start(game_board) }.to output('Welcome Max, you move first...').to_stdout
+		expect { game.start(game_board_h) }.to output('Welcome Max, you move first...').to_stdout
 	end
 
 	it 'Starting a new game generates a new board' do
-		game.start(game_board)
-		expect(game.p1_defense).to eq game_board.p1
-		expect(game.p2_board).to eq game_board.p2
+		game.start(game_board_h)
+		expect(game.p1_defense).to eq game_board_h.p1
+		expect(game.p2_defense).to eq game_board_h.p2
 	end
 
 	context 'Game started' do
-		before do
-			game.start(game_board)
-		end
 
 		context 'Possible Moves horizontally' do
+			before do
+				game.start(game_board_h)
+			end
+
 			it 'Player 1 can move a destroyer' do
 				game.p1_move('d', 'a1', 'a2')
 				expect(game.p1_defense).to eq({
@@ -67,10 +79,37 @@ describe Game do
 		end
 
 		context 'Possible Moves vertically' do
-
-			xit 'same tests as horizontal but vertical' do
+			before do
+				game.start(game_board_v)
 			end
 
+			it 'Player 1 can move a destroyer' do
+				game.p1_move('d', 'a1', 'b1')
+				expect(game.p1_defense).to eq({
+					a1:'d', b1:'d', c1:nil, d1:nil
+				})
+			end
+
+			it 'Player 1 can move a submarine' do
+				game.p1_move('s', 'a1', 'c1')
+				expect(game.p1_defense).to eq({
+					a1:'s', b1:'s', c1:'s', d1:nil
+				})
+			end
+
+			it 'Player 1 can move a cruiser' do
+				game.p1_move('c', 'a1', 'c1')
+				expect(game.p1_defense).to eq({
+					a1:'c', b1:'c', c1:'c', d1:nil
+				})
+			end
+
+			it 'Player 1 can move a battleship' do
+				game.p1_move('b', 'a1', 'd1')
+				expect(game.p1_defense).to eq({
+					a1:'b', b1:'b', c1:'b', d1:'b'
+				})
+			end
 		end
 	end
 end
