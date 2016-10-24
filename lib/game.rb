950379cc63aafe5a12a2ce3e199f4374dcee88e9
MaxProvin <maxprovin01@gmail.com>
2016-10-24 21:45:36 +0100

    p1 can set up his/her pieces horizontallly

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,46 @@
 class Game
 
-	attr_reader :player_1, :player_2, :p1_board, :p2_board
+	attr_reader :player_1, :player_2, :p1_defense, :p2_board
 
 	def initialize(player_1, player_2 = nil)
 		@player_1 = player_1
 		@player_2 if !player_2
-		@p1_board = nil
+		@p1_defense = nil
 		@p2_board = nil
 	end
 
 	def start(new_board)
-		@p1_board = new_board.p1
+		@p1_defense = new_board.p1
 		@p2_board = new_board.p2
 		print "Welcome #{@player_1}, you move first..."
 	end
 
-	def p1_destroyer(x, y)
-	end
-
-	def p1_submarine(x, y)
-	end
-
-	def p1_cruiser(x, y)
-	end
-
-	def p1_battleship(x, y)
+	def p1_move(piece, x, y)
+		if piece == 'd'
+			@p1_defense[x.to_sym] = piece
+			@p1_defense[y.to_sym] = piece
+		elsif piece == 's'
+			if x[0] == y[0]
+				for n in x[-1]..y[-1]
+					@p1_defense[(x[0] + n.to_s).to_sym] = 's'
+					@p1_defense[(x[0] + n.to_s).to_sym] = 's'
+				end
+			end
+		elsif piece == 'c'
+			if x[0] == y[0]
+				for n in x[-1]..y[-1]
+					@p1_defense[(x[0] + n.to_s).to_sym] = 'c'
+					@p1_defense[(x[0] + n.to_s).to_sym] = 'c'
+				end
+			end
+		else
+			if x[0] == y[0]
+				for n in x[-1]..y[-1]
+					@p1_defense[(x[0] + n.to_s).to_sym] = 'b'
+					@p1_defense[(x[0] + n.to_s).to_sym] = 'b'
+				end
+			end
+		end
 	end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,21 +3,14 @@ require 'game'
 describe Game do
 
 	subject(:game) { described_class.new(player_1, player_2) }
-	let(:game_board) { double :game_board, p1: {a1:nil,a2:nil,a3:nil,a4:nil,
-																							b1:nil,b2:nil,b3:nil,b4:nil,
-																							c1:nil,c2:nil,c3:nil,c4:nil},
-																				 p2: {a1:nil,a2:nil,a3:nil,a4:nil,
-																							b1:nil,b2:nil,b3:nil,b4:nil,
-																							c1:nil,c2:nil,c3:nil,c4:nil} }
+	let(:game_board) { double :game_board, p1: {a1:nil,a2:nil,a3:nil,a4:nil},
+																				 p2: {a1:nil,a2:nil,a3:nil,a4:nil} }
 	let(:player_1) { 'Max' }
 	let(:player_2) { double :player_2, name: '' }
 
 	context 'Methods' do
 		it { is_expected.to respond_to(:start) }
-		it { is_expected.to respond_to(:p1_destroyer).with(2).arguments }
-		it { is_expected.to respond_to(:p1_submarine).with(2).arguments }
-		it { is_expected.to respond_to(:p1_cruiser).with(2).arguments }
-		it { is_expected.to respond_to(:p1_battleship).with(2).arguments }
+		it { is_expected.to respond_to(:p1_move).with(3).arguments }
 	end
 
 	it 'A new game can be created with 1 player' do
@@ -34,19 +27,43 @@ describe Game do
 
 	it 'Starting a new game generates a new board' do
 		game.start(game_board)
-		expect(game.p1_board).to eq game_board.p1
+		expect(game.p1_defense).to eq game_board.p1
 		expect(game.p2_board).to eq game_board.p2
-	end
-
-	xit 'No other functionality is available till a game has been started' do
-
 	end
 
 	context 'Game started' do
 		before do
-			game.start
+			game.start(game_board)
 		end
 
-	end
+		context 'Possible Moves' do
+			it 'Player 1 can move a destroyer' do
+				game.p1_move('d', 'a1', 'a2')
+				expect(game.p1_defense).to eq({
+					a1:'d',a2:'d',a3:nil,a4:nil
+				})
+			end
 
+			it 'Player 1 can move a submarine' do
+				game.p1_move('s', 'a1', 'a3')
+				expect(game.p1_defense).to eq({
+					a1:'s',a2:'s',a3:'s',a4:nil
+				})
+			end
+
+			it 'Player 1 can move a cruiser' do
+				game.p1_move('c', 'a1', 'a3')
+				expect(game.p1_defense).to eq({
+					a1:'c',a2:'c',a3:'c',a4:nil
+				})
+			end
+
+			it 'Player 1 can move a battleship' do
+				game.p1_move('b', 'a1', 'a4')
+				expect(game.p1_defense).to eq({
+					a1:'b',a2:'b',a3:'b',a4:'b'
+				})
+			end
+		end
+	end
 end
