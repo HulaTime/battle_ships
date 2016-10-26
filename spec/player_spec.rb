da9ecce7b7f24ecdf310cc7f6bb07c1124fe843b
MaxProvin <maxprovin01@gmail.com>
2016-10-26 13:54:15 +0100

    created Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,10 @@ require 'player'
 
 describe Player do
 
+	subject(:player_1) { described_class.new('Max') }
+
+	it 'New Players should be intialized with a name' do
+		expect(player_1.name).to eq 'Max'
+	end
+
 end
