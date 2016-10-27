4abaf6ef9287004d2b04d01ffbce692b3d1f3c69
MaxProvin <maxprovin01@gmail.com>
2016-10-27 17:18:29 +0100

    interface asks for player move and fixed variable names in game_board.rb

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -10,14 +10,14 @@ class GameBoard
 
 		for letter in 'a'..'j'
 			for number in 1..10
-				@board[letter+number.to_s] = 0
+				@positions[letter+number.to_s] = 0
 			end
 		end
 	end
 
 	def display
 		row_num = 1
-		@board.each do |position, value|
+		@positions.each do |position, value|
 			@board_display += "[_]" if position.length != 3
 			if position.length == 3
 				@board_display += "[_] #{row_num}\n"
