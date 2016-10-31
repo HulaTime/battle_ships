ca215dc3be5c72df4935b3ec255e98477380b220
MaxProvin <maxprovin01@gmail.com>
2016-10-31 22:21:43 +0000

    Player can log its moves in attack and defense

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,19 @@
 class Player
 
-	attr_reader :name
+	attr_reader :name, :attack_log, :defense_log
 
 	def initialize(name)
 		@name = name
+		@attack_log = Hash.new
+		@defense_log = Hash.new
+	end
+
+	def attack(position, piece)
+		attack_log[position.to_sym] = piece
+	end
+
+	def defense(pos_1, pos_2, piece)
+		defense_log[piece.to_sym] = [pos_1, pos_2]
 	end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,18 @@ describe Player do
 		expect(player_1.name).to eq 'Max'
 	end
 
+	it 'Player should log moves made in attack' do
+		player_1.attack('b6', 'b')
+		player_1.attack('c1', 'd')
+		expect(player_1.attack_log).to eq({b6:'b', c1:'d'})
+	end
+
+	it 'Player should log its defensive positions' do
+		player_1.defense('a1', 'a4', 'b')
+		player_1.defense('b2', 'b4', 'd')
+		expect(player_1.defense_log).to eq({
+			b: ['a1', 'a4'],
+			d: ['b2', 'b4']
+			})
+	end
 end
